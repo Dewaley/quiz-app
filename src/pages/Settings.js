d50683e7a149afc0419c7877b336866242833716
Dewaley <adewaleademilua@gmail.com>
2022-05-10 23:42:21 +0100

    Axios

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import Dropdown from '../components/Dropdown';
+import useAxios from '../hooks/useAxios';
 
 const Settings = () => {
+  const {response,error,loading} = useAxios({url: 'api_category.php'})
+  if(loading) {
+    return (
+      <div
+        class='w-full flex flex-col items-center justify-center'
+      >
+        <div class='loader ease-linear rounded-full border-4 border-t-4 border-gray-200 border-t-blue-600 spin-slow h-12 w-12 mb-4'></div>
+        <h2 class='text-center text-white text-xl font-semibold'>Loading...</h2>
+        <p class='w-1/3 text-center text-white'>
+          This may take a few seconds, please don't close this page.
+        </p>
+      </div>
+    );
+  }
   return (
-    <form classname='md:w-96'>
+    <form className='md:w-96'>
       <Dropdown label={'Category'} />
       <Dropdown label={'Difficulty'} />
       <Dropdown label={'Type'} />
@@ -14,7 +29,9 @@ const Settings = () => {
         placeholder='Number of Questions'
         className='w-full py-2.5 outline-none bg-transparent border-b-2 border-b-black'
       />
-<button type="submit" className='w-full py-2 mt-2 bg-blue-500 rounded'>Get Started</button>
+      <button type='submit' className='w-full py-2 mt-2 bg-blue-500 rounded'>
+        Get Started
+      </button>
     </form>
   );
 };
